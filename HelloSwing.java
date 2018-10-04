e8d133d0d5bed4dd37bd828572e3526d6405caec
Usuario Local <sergiojh@ucm.es>
2018-10-04 19:48:02 +0200

    falta el buffer

--- HelloSwing.java
+++ HelloSwing.java
@@ -3,28 +3,19 @@ import javax.swing.JButton;
 import java.awt.event.*;
 
 public class HelloSwing extends JFrame{
-	static class MiActionListener implements ActionListener{
 
-
-		public void actionPerformed(ActionEvent e){
-			System.out.println(MSJ);
-		}
-	}
 
 	public HelloSwing(String title){
 		super(title);
+		_titulo = title;
 	}//constructora
 
 	public void init(){
 		setSize(400,400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JButton boton;
-		boton = new JButton("JAVA ES");
+		JButton boton = new JButton("Pulseme wei"); 
 		add(boton);
-
-		boton.addActionListener(new MiActionListener());
-
+		boton.addActionListener((ActionEvent e)	-> System.out.println("TURUTURU"));
 	}//init
 
 	public static void main(String[] args){
@@ -36,7 +27,7 @@ public class HelloSwing extends JFrame{
 
 		System.out.println("XD");
 	}//main
-
+	private String _titulo;
 	private final static String MSJ = "Ay lmao!!";
 
 }//class HelloSwing
--- Paint.java
+++ Paint.java
@@ -1,89 +1,86 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.*
+import java.io.*;
+
 public class Paint extends JFrame {
 
-
-	public Paint(String title){
+	public Paint(String title) {
 		super(title);
 	}
-	public void init(){
-		setSize(400,400);
+	
+	public void init() {
+		setSize(400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		try{
-		_logo = javax.imageio.ImageIO.read(new File ("prueba.png"));
-		_imageWidth = _logo.getWidth(null);
-	}
-		catch(IOException ioe){
+		
+		try {
+			_logo = javax.imageio.ImageIO.read(
+								new java.io.File("Java-logo.png") );
+			_imageWidth = _logo.getWidth(null);
+		}
+		catch(IOException ioe) {
 			System.out.println(ioe);
 		}
-		_lastFrameTime = System.nanoTime();
-}
-	public void render(Graphics g){
-		//magisterio infantil simulator
-		/*g.setColor(Color.blue);
-		g.fillRect(100,100,200,200);
-		g.setColor(new Color(255,0,0,128));
-		g.fillRect(0,0,200,200);*/
-
-		g.setColor(Color.blue);
-		g.fillRect(0,0,getWidth(),getHeight());
-		g.drawImage(_logo,(int)_x,100,null);
 		
-		/*try{
-		Thread.sleep(16);
-		}
-		catch(Exception e){
-
-		}
-		*/
-		
-
 	}
 
-	public void update(double elapsedTime){
-		_x += _vX;
-		if(_x <0){
-			_x = - _x;
-			_vX *= -1;
+	public void update(double elapsedTime) {
+		_x += _incX * elapsedTime;
+		if (_x < 0) {
+			_x = -_x;
+			_incX *= -1;
 		}
-		else if(_x + _imageWidth > getWidth() ){
+		else if (_x + _imageWidth > getWidth()) {
 			_x = 2*(getWidth() - _imageWidth) - _x;
-			_vX *= -1;
-
+			_incX *= -1;
 		}
-
 	}
-	public static void main (String [] args){
+	
+	public void render(Graphics g) {
+		// aquí pintamos
+		g.setColor(Color.blue);
+		g.fillRect(0, 0, getWidth(), getHeight());
 
+
+		g.drawImage(_logo, (int)_x, 100, null);
+		/*
+		try {
+			Thread.sleep(16);
+		}
+		catch(Exception e) {
+		}
+		*/		
+	}
+	
+	public static void main(String[] args) {
+	
 		Paint ventana = new Paint("Paint");
 		ventana.init();
 		ventana.setIgnoreRepaint(true);
 		ventana.setVisible(true);
-		long nanoElapsedTime;
-		long currentTime;
+		long lastFrameTime = System.nanoTime();
+		long currentTime, nanoElapsedTime;
 		double elapsedTime;
 		Graphics g;
-		while(true){
+		while(true) {
+			currentTime = System.nanoTime();
+			nanoElapsedTime = currentTime - lastFrameTime;
+			elapsedTime = (double) nanoElapsedTime / 1E09;
+			lastFrameTime = currentTime;
 
-		currentTime = System.nanoTime();
-		nanoElapsedTime = currentTime - _lastFrameTime;
-		elapsedTime = (double) nanoElapsedTime/1E09;
-		_lastFrameTime = currentTime;
-
-		ventana.update(elapsedTime);  //logica
-
-		g = ventana.getGraphics();
-		ventana.render(g);	//render
-		g.dispose();
-		
+			// TODO: Gestionar input (que no tenemos)
+			ventana.update(elapsedTime); // Lógica.
+			g = ventana.getGraphics();
+			ventana.render(g);
+			g.dispose();
 		}
 
 	}
+	
 	Image _logo;
-	double _x =0;
-	int _vX = 50;
-	int _imageWidth =0;
 
-	long _lastFrameTime;
+	double _x = 0;
+	int _incX = 50;
+
+	int _imageWidth;
+		
 }
